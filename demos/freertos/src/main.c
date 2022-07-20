4ae2e71558ec6dff744cc5d02b227513462f695a
Joshua Lee Ortner <jlortner@lanl.gov>
2022-07-19 18:28:41 -0600

    update

--- demos/freertos/src/main.c
+++ demos/freertos/src/main.c
@@ -1,18 +1,25 @@
+#include "pico/stdlib.h"
+
 #include <FreeRTOS.h>
 #include <task.h>
+
 #include <stdio.h>
-#include "pico/stdlib.h"
 
 void led_task()
 {   
+    const char * p_task_name = "LED_Task"; 
     const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
+    
     while (true) {
         gpio_put(LED_PIN, 1);
         vTaskDelay(100);
         gpio_put(LED_PIN, 0);
         vTaskDelay(100);
+
+        printf("Delay from task (%s)\n", p_task_name);
     }
 }
 
